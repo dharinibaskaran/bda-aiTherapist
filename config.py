5d01e1ed12a6e5cd92637fcd18a7178b9e62f76b
Dharini Baskaran <dharinibaskaran@Dharinis-MacBook-Air.local>
2024-04-28 01:21:23 -0600

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://worldof1_bigdata:minions2024*@localhost:3306/worldof1_bigdata'
+    SECRET_KEY = 'your-secret-key'
+    JWT_SECRET_KEY = 'jwt-secret-string'
+    OPENAI_API_KEY = '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'
+    # Add other configuration settings as needed
